be8e53659b1a8511176ec9e7046c0be2167b4e1a
François Pluchino <francois.pluchino@klipper.dev>
2020-05-17 17:36:32 +0200

    Add emailable model trait

---
+++ Traits/EmailableTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Component\Model\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Trait of email model.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+trait EmailableTrait
+{
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     *
+     * @Assert\Email
+     * @Assert\Type(type="string")
+     * @Assert\Length(max=180)
+     * @Assert\NotBlank
+     *
+     * @Serializer\Expose
+     */
+    private ?string $email;
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
