6bc3f4a857fd5fd2a043a4227b22d4520e46c689
François Pluchino <francois.pluchino@klipper.dev>
2020-06-16 21:20:29 +0200

    Add userable model traits

---
+++ Traits/UserableTrait.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Component\Model\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use Klipper\Component\Security\Model\UserInterface;
+use Klipper\Component\SecurityExtra\Model\Traits\UserableTrait as BaseUserableTrait;
+
+/**
+ * Trait of userable model.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+trait UserableTrait
+{
+    use BaseUserableTrait;
+
+    /**
+     * @ORM\OneToOne(
+     *     targetEntity="Klipper\Component\Security\Model\UserInterface",
+     *     inversedBy="profile",
+     *     fetch="EXTRA_LAZY",
+     *     cascade={"persist", "remove"}
+     * )
+     * @ORM\JoinColumn(onDelete="CASCADE", nullable=false)
+     */
+    protected ?UserInterface $user = null;
+}
