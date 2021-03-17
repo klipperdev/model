01476411acbee1a177d1f34762f7b08f360309a7
François Pluchino <francois.pluchino@klipper.dev>
2021-03-17 13:25:09 +0100

    Add single externable trait for model

---
+++ Traits/SingleExternalableTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Component\Model\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Trait of single externalable model.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+trait SingleExternalableTrait
+{
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(max=255)
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $externalId = null;
+
+    public function getExternalId(): ?string
+    {
+        return $this->externalId;
+    }
+
+    public function setExternalId(?string $externalId): self
+    {
+        $this->externalId = $externalId;
+
+        return $this;
+    }
+}
