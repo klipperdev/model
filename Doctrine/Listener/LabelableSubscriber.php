fb46b150f3eac13f4f7056055e2d66b373929c66
François Pluchino <francois.pluchino@klipper.dev>
2022-08-17 12:51:46 +0200

    Replace deprecated getEntityManager by getObjectManager method in events

--- Doctrine/Listener/LabelableSubscriber.php
+++ Doctrine/Listener/LabelableSubscriber.php
@@ -37,7 +37,7 @@ class LabelableSubscriber implements EventSubscriber
      */
     public function onFlush(OnFlushEventArgs $args): void
     {
-        $em = $args->getEntityManager();
+        $em = $args->getObjectManager();
         $uow = $em->getUnitOfWork();
 
         foreach ($uow->getScheduledEntityInsertions() as $entity) {
